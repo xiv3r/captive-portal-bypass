c491d7f6e51e996f1489a78b8f057169df7151f3
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-10-06 13:01:17 +0800

    Create bypass.sh

---
+++ bypass.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IP="10.0.8.141"
+MAC="20:31:1C:E0:DE:27"
+IFACE="eth0.22"
+
+iptables -t mangle -I PREROUTING 1 -s "$IP" -j MARK --set-xmark 0x50/0xffffffff
+iptables -t mangle -I PREROUTING 2 -s "$IP" -j RETURN
+iptables -I FORWARD 1 -m mac --mac-source "$MAC"  -j ACCEPT
+iptables -I FORWARD 2 -d "$IP" -j ACCEPT
+iptables -t mangle -I POSTROUTING 1 -d "$IP" -o "$IFACE" -j RETURN
